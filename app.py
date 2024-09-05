c64175cf825d8999f79fee706088acf48174b0f1
Orion <emmanuel3010t@gmail.com>
2024-09-04 21:29:49 -0300

    GCP

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
 from flask import Flask, render_template
-import mysql.connector
-import sqlalchemy
+import sqlalchemy, pymysql, os
 from google.cloud.sql.connector import Connector
-import pymysql
+
 
 # Crear una aplicación Flask
 app = Flask(__name__)
 
-# Configura la conexión a Cloud SQL
+# Configuración de conexión a Cloud SQL
 def get_connection():
-    instance_connection_name = "mundofarma:us-central1:sqlmundofarma"
-    
-    # Usuario, contraseña y nombre de la base de datos en Cloud SQL
-    db_user = "root"
-    db_pass = "Elekna.24#"
-    db_name = "mundofarma"
+    instance_connection_name = os.getenv("INSTANCE_CONNECTION_NAME")
+    db_user = os.getenv("DB_USER")
+    db_pass = os.getenv("DB_PASS")
+    db_name = os.getenv("DB_NAME")
 
-    # Crea un conector de Cloud SQL
+    # Crear conector de Cloud SQL
     connector = Connector()
 
-    # Devuelve una conexión a la base de datos usando PyMySQL
+    # Retornar la conexión a la base de datos
     connection = connector.connect(
         instance_connection_name,
         "pymysql",
@@ -29,26 +26,15 @@ def get_connection():
     )
     return connection
 
-# Ruta para la página principal
 @app.route('/')
 def mostrar_productos():
-    # Obtener una conexión a la base de datos
     connection = get_connection()
-
-    # Crear un cursor para ejecutar consultas
     cursor = connection.cursor()
-
-    # Consulta SQL para obtener los datos
     consulta_sql = "SELECT `ID_Producto`, `Nombre_Producto`, `Componente_Producto` FROM producto;"
-
     cursor.execute(consulta_sql)
-
-    # Obtener todos los resultados de la consulta
     resultados = cursor.fetchall()
-
     cursor.close()
     connection.close()
-
     return render_template('MundoFarma.html', productos=resultados)
 
 if __name__ == '__main__':
